a68954ada327459cdfc5b7522c143c8752a850d8
Dan Langevin <dan.langevin@lifebooker.com>
2011-10-10 12:28:22 -0400

    added simplecov

--- Gemfile
+++ Gemfile
@@ -16,6 +16,6 @@ group :development do
   gem "guard-rspec"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.6.4"
-  gem "simple_cov"
+  gem "simplecov"
   gem "mocha"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,23 @@
+require 'simplecov'
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'sailthru_mailer'
 require 'ruby-debug'
 
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Bundler.require(:default, :test)
 Debugger.start
 
-Bundler.require(:defaults, :test)
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+SimpleCov.at_exit do
+  SimpleCov.result.format!
+end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
